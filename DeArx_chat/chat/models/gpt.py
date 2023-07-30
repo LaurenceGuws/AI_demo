e50d4c459c1ddfc52008e44cf4e1269b7d74feff
LaurenceGouws <lggousie46@gmail.com>
2023-07-30 10:35:29 +0200

    Done with Dockerization and k8s poc

--- DeArx_chat/chat/models/gpt.py
+++ DeArx_chat/chat/models/gpt.py
@@ -1,15 +1,11 @@
 import os
 import openai
 import base64
-import configparser
 
 class Chat:
     @staticmethod
     def chat_gpt_interact(messages):
-        # Read the API key from the config file and decode it
-        config = configparser.ConfigParser()
-        config.read('conf\config.conf')
-        openai_key = os.getenv('OPENAI_API_KEY')
+        openai_key = base64.b64decode(os.getenv('OPENAI_API_KEY')).decode()
         openai.api_key = openai_key
 
         # Call the GPT model
