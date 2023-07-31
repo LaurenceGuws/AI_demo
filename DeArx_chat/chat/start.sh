495f638ee8952e751e8f4d98925aad266de2a533
Laurence Gouws <lgouws@dearx.ca.za>
2023-07-31 12:08:31 +0200

    k8s working

--- DeArx_chat/chat/start.sh
+++ DeArx_chat/chat/start.sh
@@ -1,5 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+# Define a function to print a message within lines of '#' characters
+print_message() {
+    echo "################################################################################"
+    echo " $1"
+    echo "################################################################################"
+}
+
+print_message "Container starting..."
+
+# Write environment variables to /etc/environment
+printenv > /etc/environment
+
+# Run the database initialization
 if [ ! -f /app/instance/chat.db ]; then
-    flask setup
+    flask setup >> /proc/1/fd/1 2>> /proc/1/fd/2
+    print_message "Starting database initialization..."
+    print_message "Database initialization completed."
+
+# Run the application
 fi
-flask run --debug --host=0.0.0.0 --port=5000
+print_message "Starting the application..."
+flask run --debug --host=0.0.0.0 --port=5000 >> /proc/1/fd/1 2>> /proc/1/fd/2
+print_message "Application has started."
+
