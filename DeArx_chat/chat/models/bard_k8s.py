27835dc8bd1aeeabe5730510b8c73d21a7540727
LaurenceGouws <lggousie46@gmail.com>
2023-08-06 23:11:46 +0200

    WIP

--- DeArx_chat/chat/models/bard_k8s.py
+++ DeArx_chat/chat/models/bard_k8s.py
@@ -1,10 +1,31 @@
+import os
+import base64
+import json
 import vertexai
 from vertexai.preview.language_models import ChatModel, InputOutputTextPair
 
+def get_service_account_key_from_env():
+    """
+    Decodes the service account key from the environment variable and returns it as a dictionary.
+    """
+    encoded_key = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON_BASE64", "")
+    decoded_key = base64.b64decode(encoded_key).decode('utf-8')
+    return json.loads(decoded_key)
+
+def setup_google_auth():
+    """
+    Sets up the environment for Google Cloud authentication using the service account key from the environment variable.
+    """
+    service_account_info = get_service_account_key_from_env()
+    temp_path = "/tmp/service_account.json"
+    
+    with open(temp_path, "w") as file:
+        json.dump(service_account_info, file)
+    
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp_path
+
 def interact(messages):
-
-    # return None
-
+    setup_google_auth()  # Set up authentication at the beginning of the function
     project_id='palm-386622'
     model_name='chat-bison@001'
     temperature=0.5
